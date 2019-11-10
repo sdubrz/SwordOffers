74d7dd6f2359715e22b4702099091196b6d00458
sdu20 <sdu2014@126.com>
2019-11-10 22:16:59 +0800

    search target in 2d array

---
+++ src/ArrayTest/Search2dArray.java
@@ -0,0 +1,77 @@
+package ArrayTest;
+
+public class Search2dArray {
+	
+	/***
+	 * 应该用递归的方法
+	 * @param target
+	 * @param array
+	 * @return
+	 */
+	public boolean Find(int target, int [][] array) {
+		int n = array.length;
+		int m = array[0].length;
+		if(n==1 && m==1) {
+			if(array[0][0]==target) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+		if(array[0][0] > target) {
+			return false;
+		}
+		if(array[n-1][m-1]<target) {
+			return false;
+		}
+		
+		int n_point = n/2;
+		int m_point = m/2;
+		if(array[n_point][m_point]==target) {
+			return true;
+		}else if(array[n_point][m_point]<target){
+			int[][] right_array = new int[n][m-m_point];  // 右方的矩阵
+			int[][] down_array = new int[n-n_point][m];  // 下方的矩阵
+			for(int i=0; i<n;i++) {
+				for(int j=0; j<m-m_point; j++) {
+					right_array[i][j] = array[i][m_point+j];
+				}
+			}
+			for(int i=0; i<n-n_point; i++) {
+				for(int j=0; j<m; j++) {
+					down_array[i][j] = array[n_point+i][j];
+				}
+			}
+			return this.Find(target, right_array) || this.Find(target, down_array);
+		}else if(array[n_point][m_point]>target) {
+			int[][] left_array = new int[n][m_point+1];  // 左方的矩阵
+			int[][] up_array = new int[n_point+1][m];  //  上方的矩阵
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<m_point+1; j++) {
+					left_array[i][j] = array[i][j];
+				}
+			}
+			for(int i=0; i<n_point+1; i++) {
+				for(int j=0; j<m; j++) {
+					up_array[i][j] = array[i][j];
+				}
+			}
+			return this.Find(target, left_array) || this.Find(target, up_array);
+		}
+		
+		//System.out.println("no found");
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] numbers = {{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+				{14, 15, 16, 17}};
+		int[][] array = {numbers[3]};
+		Search2dArray obj = new Search2dArray();
+		System.out.println(obj.Find(7, numbers));
+	}
+
+}
--- src/Search2dArray/Solution.java
+++ src/Search2dArray/Solution.java
@@ -2,16 +2,103 @@ package Search2dArray;
 
 public class Solution {
 	
+	/***
+	 * Search a number in a sorted 2-d array
+	 * @param target
+	 * @param array
+	 * @return
+	 */
 	public boolean Find(int target, int [][] array) {
+		int n = array.length;
+		int m = array[0].length;
+		
+		int n_low = 0;
+		int n_high = n - 1;
+		int m_low = 0;
+		int m_high = m - 1;
+		
+		// 先判断应该在哪一行
+		while(n_low < n_high){
+			if(n_high - n_low == 1) {
+				if(array[n_low][m-1] >= target) {
+					n_high = n_low;
+				}else {
+					n_low = n_high;
+				}
+				break;
+			}
+			int temp_n = (n_high+n_low)/2;
+			if(array[temp_n][0]>target) {
+				n_high = temp_n;
+			}else if(array[temp_n][m-1]<target) {
+				n_low = temp_n;
+			}else {
+				n_low = temp_n;
+				n_high = temp_n;
+				break;
+			}
+		}
+		
+		if(array[n_low][m-1]<target || array[n_low][0]>target) {
+			return false;
+		}
+		
+		while(m_low < m_high) {
+			if(m_low+1==m_high) {
+				if(array[n_low][m_low]==target || array[n_low][m_high]==target) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+			int temp_m = (m_low+m_high) / 2;
+			if(array[n_low][temp_m]==target) {
+				return true;
+			}else if(array[n_low][temp_m] > target) {
+				m_high = temp_m;
+			}else {
+				m_low = temp_m;
+			}
+		}
 		
 		
 		return false;
 	}
-
+	
+	public boolean Find1(int target, int [][] array) {
+		int n = array.length;
+		int m = array[0].length;
+		
+		int n_low = 0;
+		int n_up = n;
+		int m_low = 0;
+		int m_up = m;
+		
+		while(n_low<n_up-1) {
+			int point = (n_low+n_up) / 2;
+			if(array[point][0]>=target) {
+				
+			}
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {1, 2, 3};
-		System.out.println(a.length);
+		Solution s = new Solution();
+		int[][] numbers = {{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+				{14, 15, 16, 17}};
+		boolean temp_bool = s.Find(9,  numbers);
+		System.out.println(temp_bool);
+		for(int i=0; i<20;i++) {
+			System.out.print(i+" \t");
+			System.out.println(s.Find(i, numbers));
+		}
+		//System.out.println(s.Find(5, numbers));
+		//System.out.println(numbers.length);
 	}
 
 }
