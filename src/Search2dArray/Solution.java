92b989c30f5cac39d4a41648bbd7b055b118a5c6
sdu20 <sdu2014@126.com>
2019-11-10 16:38:39 +0800

    search data in 2-d array

---
+++ src/Search2dArray/Solution.java
@@ -0,0 +1,17 @@
+package Search2dArray;
+
+public class Solution {
+	
+	public boolean Find(int target, int [][] array) {
+		
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1, 2, 3};
+		System.out.println(a.length);
+	}
+
+}
